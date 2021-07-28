5e9cf6a6b5d059127703548f19f834ca8db2549c
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-07-28 21:32:37 +0100

    Update build led for day and night

--- Build-LED/main.py
+++ Build-LED/main.py
@@ -10,8 +10,11 @@ class Led:
     def __init__(self):
         self.spacing = 360.0 / 16.0
         self.hue = 0
-        blinkt.set_brightness(0.1)
+        self.brightness = 0.05
         self.time_allowed = True
+        blinkt.clear()
+        blinkt.show()
+        blinkt.set_clear_on_exit(True)
 
     def within_time(self):
         check_time = self.local.utcnow().time()
@@ -21,20 +24,28 @@ class Led:
         end   = check_time <= end_time
         self.time_allowed = begin and end
 
-    def run_lights(self):
+    def run_night(self):
+        blinkt.set_all(100, 100, 100, self.brightness)
+        blinkt.show()
+
+    def run_day(self):
         self.hue = int(time.time() * 100) % 360
         for x in range(8):
             offset = x * self.spacing
             h = ((self.hue + offset) % 360) / 360.0
             r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
-            blinkt.set_pixel(x, r, g, b)
+            blinkt.set_pixel(x, r, g, self.brightness)
         blinkt.show()
-        time.sleep(0.001)
+        time.sleep(0.05)
 
     def startup(self):
         while self.time_allowed:
             self.within_time()
-            run_lights()
+            run_day()
+        while not self.time_allowed:
+            self.within_time()
+            run_night()
+
 
 if __name__ == "__main__":
     while True:
