4be3cf28273b2d1126a2fa3d74483a300170f854
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-07-28 21:52:10 +0100

    Update with logging

--- Build-LED/main.py
+++ Build-LED/main.py
@@ -8,6 +8,7 @@ import colorsys
 class Led:
 
     def __init__(self):
+        print('__init__()')
         self.spacing = 360.0 / 16.0
         self.hue = 0
         self.brightness = 0.1
@@ -17,6 +18,7 @@ class Led:
         blinkt.set_clear_on_exit(True)
 
     def within_time(self):
+        print('within_time()')
         check_time = datetime.datetime.utcnow().time()
         begin_time = datetime.time(8,00)
         end_time   = datetime.time(20,00)
@@ -25,21 +27,30 @@ class Led:
         self.time_allowed = begin and end
 
     def run_night(self):
+        print('run_night()')
         blinkt.set_all(100, 100, 100, self.brightness)
         blinkt.show()
         time.sleep(0.05)
 
     def run_day(self):
-        self.hue = int(time.time() * 100) % 360
+        print('run_day()')
+        hue = int(time.time() * 100) % 360
         for x in range(8):
             offset = x * self.spacing
-            h = ((self.hue + offset) % 360) / 360.0
+            h = ((hue + offset) % 360) / 360.0
             r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
             blinkt.set_pixel(x, r, g, b)
+            print('Setting pixel: {} to R: {}, G: {}, B: {}'.format(
+                x,
+                r,
+                g,
+                b
+            ))
         blinkt.show()
         time.sleep(0.001)
 
     def startup(self):
+        print('startup()')
         while not self.time_allowed:
             self.within_time()
             self.run_day()
@@ -49,6 +60,6 @@ class Led:
 
 
 if __name__ == "__main__":
-    while True:
-        led = Led()
-        led.startup()
+    print('Starting Program')
+    led = Led()
+    led.startup()
