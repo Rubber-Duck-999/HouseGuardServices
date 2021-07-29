2563aa7efcc55faacf662fad9599618936b068b3
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-07-29 21:45:45 +0100

    Try threading

--- Build-LED/main.py
+++ Build-LED/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''To set up the led array'''
 import blinkt
+from threading import Thread
 import time
 import colorsys
 from network_test import NetworkTest, Colours
@@ -9,53 +10,55 @@ class Led:
 
     def __init__(self):
         print('__init__()')
-        self.brightness = 0.05
+        self.brightness = 0.01
         self.pixels = 8
-        self.time_allowed = True
         self.network_test = NetworkTest()
+        self.colour = Colours.Red
         blinkt.clear()
         blinkt.show()
         blinkt.set_clear_on_exit(True)
 
-    def set_pixels(self, colour, x):
+    def set_pixels(self, x):
         print('set_pixels()')
         blue    = 0
         green   = 0
         red     = 0
         varying = x * 5
-        if colour == Colours.Red:
+        if self.colour == Colours.Red:
             # Red
             red  = 220 + varying
-        elif colour == Colours.Yellow:
+        elif self.colour == Colours.Yellow:
             # Yellow
             red   = 220 + varying
             green = 66
-        elif colour == Colours.Green:
+        elif self.colour == Colours.Green:
             # Green
             green = 220 + varying
-        for x in range(self.pixels):
-            blinkt.set_pixel(x, red, green, blue, self.brightness)
+        blinkt.set_pixel(x, red, green, blue, self.brightness)
+        blinkt.show()
 
-    def run(self, colour):
-        print('run()')
-        x = 0
-        while x < 5:
-            self.set_pixels(colour, x)
-            blinkt.show()
-            time.sleep(5)
-            self.set_pixels(colour, x)
-            blinkt.show()
-            time.sleep(5)
-            x += 1
+    def run_lights(self):
+        print('run_lights()')
+        while True:
+            pixel = 0
+            while pixel < self.pixels:
+                self.set_pixels(pixel)
+                time.sleep(0.1)
+                pixel += 1
 
-    def startup(self):
-        print('startup()')
-        colour = self.network_test.check_speed()
-        self.run(colour)
+    def check_network(self):
+        print('check_network()')
+        while True:
+            self.colour = self.network_test.check_speed()
+            time.sleep(10)
 
 
 if __name__ == "__main__":
     print('Starting Program')
     led = Led()
-    while True:
-        led.startup()
+    T1 = Thread(target=Led().run_lights, args=())
+    T2 = Thread(target=Led().check_network, args=())
+    T1.start()
+    T2.start()
+    T1.join()
+    T2.join()
