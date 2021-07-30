099dc7a51378da6ebb8366a290aa9db332350036
Rubberduck <simon.crowther95@hotmail.co.uk>
2021-07-30 13:30:28 +0100

    Update run.sh

--- Build-LED/run.sh
+++ Build-LED/run.sh
@@ -2,4 +2,4 @@
 
 echo 'Starting'
 sleep 20
-python3 /home/pi/Documents/HouseGuardServices/Build-LED/main.py 2&>1 /home/pi/Documents/run.log
+python3 /home/pi/Documents/HouseGuardServices/Build-LED/main.py /home/pi/Documents/run.log 2&>1
