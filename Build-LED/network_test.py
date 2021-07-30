d718a40e89d3ec237548d90afcadfdd387699780
Rubberduck <simon.crowther95@hotmail.co.uk>
2021-07-30 15:09:08 +0100

    Update network_test.py

--- Build-LED/network_test.py
+++ Build-LED/network_test.py
@@ -17,26 +17,30 @@ class NetworkTest:
     def __init__(self):
         '''Constructor for class'''
         self.speed = speedtest.Speedtest()
-        self.down  = 0
+        self.down   = 0
+        self.red    = 60
+        self.orange = 90
+        self.purple = 120
+        self.green  = 150
 
     def check_speed(self):
         '''Check speed of both checks'''
-        red    = 60
-        orange = 90
-        purple = 120
-        green  = 150
-        down_speed = self.speed.download() / 1048576
-        down = round(down_speed)
+        down = self.red
+        try:
+            down_speed = self.speed.download() / 1048576
+            down = round(down_speed)
+        except SpeedtestException as error:
+            print('Error occurred')
         if self.down != down:
             print('Speed changed: {}'.format(down))
             self.down = down
-        if self.down <= red:
+        if self.down <= self.red:
             return Colours.Red
-        if self.down <= orange:
+        if self.down <= self.orange:
             return Colours.Orange
-        if self.down <= purple:
+        if self.down <= self.purple:
             return Colours.Purple
-        if self.down <= green:
+        if self.down <= self.green:
             return Colours.Green
         return Colours.Blue
 
