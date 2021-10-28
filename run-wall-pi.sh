feefdff6e284b67d2cca152a44606847cf766e01
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-10-28 14:51:24 +0100

    Swap notifier out

--- run-wall-pi.sh
+++ run-wall-pi.sh
@@ -4,5 +4,3 @@ echo 'Starting'
 python3 /home/pi/Documents/HouseGuardServices/Motion/main.py &
 
 python3 /home/pi/Documents/HouseGuardServices/Status-GUI/main.py &
-
-python3 /home/pi/Documents/HouseGuardServices/Notifier/app.py &
