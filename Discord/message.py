93e631dcb0f05e8114ec6d5df0dd46610cb2b76f
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-10-28 08:49:50 +0100

    Updated set of messages

--- Discord/message.py
+++ Discord/message.py
@@ -1,8 +1,26 @@
+import random
 
 class MessageManager:
     
     def __init__(self, message):
         self.message = message
+        self.messages = []
+
+    def get_status(self):
+        self.messages = [
+            {
+                'name': '$Devices',
+                'message': 'Devices online: {}'.format(random.randint(0, 20))
+            },
+            {
+                'name': '$Allowed',
+                'message': 'Devices Allowed online: {}'.format(random.randint(0, 20))
+            },
+        ]
 
     def get_message(self):
+        self.get_status()
+        for message in self.messages:
+            if message['name'] == self.message:
+                return message['message']
         return 'Test'
