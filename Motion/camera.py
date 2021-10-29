ab5a8f44a181770b4600ae2c042522512676f8e3
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-10-29 13:21:28 +0100

    Try using a different image

--- Motion/camera.py
+++ Motion/camera.py
@@ -59,7 +59,8 @@ class Camera:
                 if self.motion_counter >= 8:
                     # check to see if we should take pictures
                     logging.info("Motion detected")
-                    image = "{}/img.jpg".format('/home/pi/Desktop/cam_images')
+                    current = self.timestamp.strftime("%d:%m:%Y %H:%M:%S")
+                    image = "{}/{}.jpg".format('/home/pi/Desktop/cam_images', current)
                     logging.info("Creating file: {}".format(image))
                     cv2.imwrite(image, frame)
                     colorImage  = Image.open(image)
