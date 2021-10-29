6f596cf03626e7dd933ad4ead2d4b58737a2231d
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-10-29 15:58:22 +0100

    Temporailty disable gui

--- run-wall-pi.sh
+++ run-wall-pi.sh
@@ -3,4 +3,4 @@
 echo 'Starting'
 python3 /home/pi/Documents/HouseGuardServices/Motion/main.py &
 
-python3 /home/pi/Documents/HouseGuardServices/Status-GUI/main.py &
+# python3 /home/pi/Documents/HouseGuardServices/Status-GUI/main.py &
