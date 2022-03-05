7de3a3323ff6b077e04e73c96b12f2114e0c710d
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2022-03-05 14:44:48 +0000

    Fix sizes again

--- Alarm/main.py
+++ Alarm/main.py
@@ -25,9 +25,9 @@ def my_long_operation():
 
 sg.theme('DarkAmber')   # Add a touch of color
 entry = pin.Pin()
-x = 16
+x = 12
 y = 5
-font = ("Helvetica", 14)
+font = ("Helvetica", 40)
 layout = [
 	[sg.Text('Enter your Pin: ', size=(40, 1), font=font), sg.Text(size=(40,1), font=font, key='-OUTPUT-')],
 	[sg.ReadButton('1', size=(x, y), font=font), sg.ReadButton('2', size=(x, y), font=font), sg.ReadButton('3', size=(x, y), font=font)],
