e32c99f2be5ccccf84667a042c67bf0a4be9220c
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2022-06-27 11:59:21 +0100

    Update time piece

--- Mood/main.py
+++ Mood/main.py
@@ -6,6 +6,9 @@ from colorsys import hsv_to_rgb
 from unicornhatmini import UnicornHATMini
 import utilities
 import logging
+from suntime import Sun, SunTimeException
+import datetime
+
 
 filename = '/home/{}/sync/mood.log'
 
@@ -25,6 +28,23 @@ logging.info('Starting Program')
 
 unicornhatmini = UnicornHATMini()
 unicornhatmini.set_brightness(0.1)
+
+
+latitude = 51.86422539581071
+longitude = -2.2411648453595343
+
+sun = Sun(latitude, longitude)
+
+# Get today's sunrise and sunset in UTC
+today_sr = sun.get_sunrise_time()
+today_ss = sun.get_sunset_time()
+print('Today at GB the sun raised at {} and get down at {} UTC'.format(today_sr.strftime('%H:%M'), today_ss.strftime('%H:%M')))
+
+# On a special date in your machine's local time zone
+abd = datetime.date(2014, 10, 3)
+abd_sr = sun.get_local_sunrise_time(abd)
+abd_ss = sun.get_local_sunset_time(abd)
+print('On {} the sun at Warsaw raised at {} and get down at {}.'.format(abd, abd_sr.strftime('%H:%M'), abd_ss.strftime('%H:%M')))
 
 while True:
     hue = (time.time() / 10.0)
