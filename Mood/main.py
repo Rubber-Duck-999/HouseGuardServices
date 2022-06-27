7ca3f9e6616fe97f80102acfbd6e16b06fb2d194
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2022-06-27 12:00:10 +0100

    Update piece

--- Mood/main.py
+++ Mood/main.py
@@ -38,13 +38,7 @@ sun = Sun(latitude, longitude)
 # Get today's sunrise and sunset in UTC
 today_sr = sun.get_sunrise_time()
 today_ss = sun.get_sunset_time()
-print('Today at GB the sun raised at {} and get down at {} UTC'.format(today_sr.strftime('%H:%M'), today_ss.strftime('%H:%M')))
-
-# On a special date in your machine's local time zone
-abd = datetime.date(2014, 10, 3)
-abd_sr = sun.get_local_sunrise_time(abd)
-abd_ss = sun.get_local_sunset_time(abd)
-print('On {} the sun at Warsaw raised at {} and get down at {}.'.format(abd, abd_sr.strftime('%H:%M'), abd_ss.strftime('%H:%M')))
+logging.info('Today at GB the sun raised at {} and get down at {} UTC'.format(today_sr.strftime('%H:%M'), today_ss.strftime('%H:%M')))
 
 while True:
     hue = (time.time() / 10.0)
